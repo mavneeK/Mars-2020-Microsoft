c0e3c4f4804035a9dcd85444aa239d03c88d3ed1
Mavneet <mav.kaur17@gmail.com>
2020-06-28 15:32:51 +0530

    Added dijkstra algorithm

---
+++ mars-rover/src/Algorithms/dijkstra.js
@@ -0,0 +1,57 @@
+export function dijkstra(grid, startNode, finishNode) {
+   const visitedNodesInOrder = [];
+   startNode.distance = 0;
+   const unvisitedNodes = getAllNodes(grid);
+   while (!!unvisitedNodes.length) {
+       sortNodes(unvisitedNodes);
+       const closestNode = unvisitedNodes.shift();
+       if (closestNode.isWall) continue;
+
+       if (closestNode.distance === Infinity) return visitedNodesInOrder;
+       closestNode.isVisited = true;
+       visitedNodesInOrder.push(closestNode);
+       if (closestNode === finishNode) return visitedNodesInOrder;
+       updateUnvisitedNeighbors(closestNode, grid);
+   }
+}
+
+function sortNodes(unvisitedNodes) {
+   unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+}
+
+function updateUnvisitedNeighbors(node, grid) {
+   const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+   for (const neighbor of unvisitedNeighbors) {
+       neighbor.distance = node.distance + 1;
+       neighbor.previousNode = node;
+   }
+}
+
+function getUnvisitedNeighbors(node, grid) {
+   const neighbor = [];
+   const { col, row } = node;
+   if (row > 0) neighbor.push(grid[row - 1][col]);
+   if (row < grid.length - 1) neighbor.push(grid[row + 1][col]);
+   if (col > 0) neighbor.push(grid[row][col - 1]);
+   if (col < grid[0].length - 1) neighbor.push(grid[row][col + 1]);
+   return neighbor.filter(neighbor => !neighbor.isVisited);
+}
+function getAllNodes(grid) {
+   const nodes = [];
+   for (const row of grid) {
+       for (const node of row) {
+           nodes.push(node);
+       }
+   }
+   return nodes;
+}
+
+export function getNodesInShortestPathOrder(finishNode) {
+   const nodesInShortestPathOrder = [];
+   let currentNode = finishNode;
+   while (currentNode !== null) {
+       nodesInShortestPathOrder.unshift(currentNode);
+       currentNode = currentNode.previousNode;
+   }
+   return nodesInShortestPathOrder;
+}
